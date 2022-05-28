20593d6c1b7f46ee8e81c5cd248424ba0b241904
Raruto <Raruto@users.noreply.github.com>
2022-05-28 09:00:48 +0200

    short circuit expression

--- src/index.js
+++ src/index.js
@@ -135,7 +135,6 @@ export var GestureHandling = L.Handler.extend({
 		(this._isLanguageContent(opts.text) ? Promise.resolve(opts.text) : this._getLanguageContent(opts.locale)).then((content) => {
 			this._map._container.setAttribute("data-gesture-handling-touch-content", content.touch);
 			this._map._container.setAttribute("data-gesture-handling-scroll-content", content.scroll);
-
 			this._touchWarning = content.touch;
 			this._scrollWarning = content.scroll;
 		});
@@ -149,7 +148,6 @@ export var GestureHandling = L.Handler.extend({
 	_getLanguageContent: function(lang) {
 		//Determine user language (eg. fr or en-US)
 		lang = lang || this._getUserLanguage() || "en";
-
 		var resolve, promise = new Promise(_resolve => { resolve = _resolve });
 		var consume = (m) => {
 			var content = m.default || {};
@@ -169,24 +167,11 @@ export var GestureHandling = L.Handler.extend({
 		return promise;
 	},
 
-	_hasClass: function(element, classList) {
-		for (var i = 0; i < classList.length; i++) {
-			if (L.DomUtil.hasClass(element, classList[i])) {
-				return true;
-			}
-		}
-		return false;
-	},
-
 	_handleTouch: function(e) {
-		//Disregard touch events on the minimap if present
-		//var ignore = this._hasClass(e.target, ["leaflet-control-minimap", "leaflet-interactive", "leaflet-popup-content", "leaflet-popup-content-wrapper", "leaflet-popup-close-button", "leaflet-control-zoom-in", "leaflet-control-zoom-out"]);
-		var ignore = this._hasClass(e.target, ["leaflet-interactive"]);
-		var ignore_control = e.target.className.indexOf("leaflet-control");
-		var ignore_popup = e.target.className.indexOf("leaflet-popup");
+		//Disregard touch events on the controls and popups if present
+		var ignore = L.DomUtil.hasClass(e.target, "leaflet-interactive") || e.target.closest('.leaflet-control-container') || e.target.closest('.leaflet-popup-pane');
 
-		//if (ignore) {
-		if (ignore || ignore_control !== -1 || ignore_popup !== -1) {
+		if (ignore) {
 			if (L.DomUtil.hasClass(e.target, "leaflet-interactive") && e.type === "touchmove" && e.touches.length === 1) {
 				this._enableTouchWarning();
 			} else {
